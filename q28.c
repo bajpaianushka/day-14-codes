565833976bc7b1b2df9a2365276330080c29ec24
bajpaianushka <anushka.39934@stu.upes.ac.in>
2026-09-06 14:17:46 +0530

    Add files via upload

---
+++ q28.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+	int n = 4;
+	scanf("%d", &n);
+
+	long long product = 1;
+	int hasEven = 0;
+
+	for(int i=1; i<=n; i++){
+		if(i % 2 == 0){
+			product = product*i;
+			hasEven = 1;
+		}
+	}
+
+	if(hasEven)
+		printf("%lld", product);
+	else
+		printf("0");
+
+	return 0;
+}
